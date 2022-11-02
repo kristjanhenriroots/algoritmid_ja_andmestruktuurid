a8d79cb6ee2f604538868955be263ecfaf98fa47
kristjanhenriroots <85015619+kristjanhenriroots@users.noreply.github.com>
2022-11-02 15:00:06 +0200

    Ready for defence

--- Main.cpp
+++ Main.cpp
@@ -8,16 +8,13 @@
 #include "time.h"
 #pragma warning ( disable : 4996 )
 
-void PrintObjects(HeaderD* pStruct7)		// function to print all data
-{
+void PrintObjects(HeaderD* pStruct7) {		// function to print all data
 	
-
 	HeaderD* pStructNew = pStruct7;			// copying input data to local variable
 	Object3* obj;							// creating struct pointer where data is stored
 	int i = 1, j;							// data counters
 			
-	while (pStructNew) 
-	{
+	while (pStructNew) {
 		obj = (Object3*)pStructNew->pObject;			// typecasting void pointer to correct struct
 		printf("%2d. %c\n", i, pStructNew->cBegin);
 		j = 1;
@@ -43,6 +40,7 @@ int checkFormat(char* name) {
 	return 1;
 }
 
+// function to find the correct headerD element
 HeaderD* beginIndex(HeaderD* pStruct7, char start) {
 	int loop = 0;
 	while (start > pStruct7->cBegin) {						// loop the structure as long as char start is smaller than the current cBegin
@@ -51,24 +49,24 @@ HeaderD* beginIndex(HeaderD* pStruct7, char start) {
 		pStruct7 = pStruct7->pNext;							// and there is a next element, the loop ends when start and cBegin match
 		loop++;
 	}
-	if (start == pStruct7->cBegin)
+	if (start == pStruct7->cBegin)							// if the correct header is found it will be returned
 		return pStruct7;
 
 	HeaderD* pStructNew;
 
 	pStructNew = (HeaderD*)malloc(sizeof(HeaderD));		// give memory to new element
 	if (pStructNew == NULL)								// memory allocation error
-		return 0;
+		return NULL;
 
-	printf("%c: created memory for new element\n", start);
+	//printf("%c: created memory for new element\n", start);
 
-	if (start < pStruct7->cBegin && loop == 0) {						// edge case, new cBegin is sooner in the alphabet than the first element in struct
-		printf("new element is first in list\n");
+	if (start < pStruct7->cBegin && loop == 0) {		// edge case, new cBegin is sooner in the alphabet than the first element in struct
+		//printf("new element is first in list\n");
 		pStructNew->pNext = pStruct7;
 		pStructNew->pPrior = NULL;
 		pStruct7->pPrior = pStructNew;
 	}
-	else if (start > pStruct7->cBegin) {			// edge case 2, new cBegin is last in array
+	else if (start > pStruct7->cBegin) {				// edge case 2, new cBegin is last in array
 		pStructNew->pPrior = pStruct7;
 		pStruct7->pNext = pStructNew;
 		pStructNew->pNext = NULL;
@@ -93,50 +91,52 @@ HeaderD* beginIndex(HeaderD* pStruct7, char start) {
 	return pStructNew;
 }
 
+enum object_states{none, present, first = -1};	// enum to describe the exists variable states in findObject()
+
 // function to find if the given pNewID already exists in the array, returns NULL if it is not present
 Object3* findObject(Object3* obj, char* pNewID, int* exists) {
-	while (obj != NULL && strcmp(pNewID, obj->pID) >= 0) {	// since the list is sorted there is no need to check objects that come later in the alphabet
+	while (obj && strcmp(pNewID, obj->pID) >= 0) {	// since the list is sorted there is no need to check objects that come later in the alphabet
 		//printf("%s and %s\n", pNewID, obj->pID);
 		if (!strcmp(pNewID, obj->pID)) {
-			*exists = 1;
+			*exists = present;
 			//printf("Found in array\n");
 			return obj;						// pNewID is already in the array
 		}
 			
-		if (obj->pNext == NULL || strcmp(pNewID, obj->pNext->pID) < 0)
+		if (!obj->pNext || strcmp(pNewID, obj->pNext->pID) < 0)
 			break;
 		obj = obj->pNext;
 	}
-	if (obj == NULL || strcmp(pNewID, obj->pID) < 0)
-		*exists = -1;
+	if (!obj || strcmp(pNewID, obj->pID) < 0)	// object doesn't exist in list and would need to be first
+		*exists = first;
 	else
-		*exists = 0;
+		*exists = none;
 	//printf("\nEnd of function object3 %s\n", obj->pID);
 	return obj;							// pNewID is not present
 }
 
-unsigned long int randomNumber() {
-	return rand() * 30;	// RAND_MAX is 32767, so the result is multiplied by 30 to get a 9 digit number as a limit
-}
-
 int InsertNewObject(HeaderD** pStruct7, char* pNewID, unsigned long int NewCode) {
+	
 	HeaderD* pS7 = *pStruct7;
 	
 	if (!checkFormat(pNewID))	// check the format of pNewID
 		return 0;
 	pS7 = beginIndex(pS7, pNewID[0]);	// find cBegin, if none make new
-	
+	if (pS7 == NULL)
+		return 0;
+
+
 	Object3* obj_test;
 	obj_test = (Object3*)pS7->pObject;
 	
 	// check if pNewID exists
 	Object3* newObj, *foundObj, *nextObj;
-	int exists = -1;
+	int exists = first;
 	int* pExists = &exists;
 	foundObj = findObject((Object3*)pS7->pObject, pNewID, pExists);
 	
-	if (exists == 1) {
-		printf("ID %s already present\n", pNewID);
+	if (exists == present) {
+		printf("ID %s already present, ", pNewID);
 		return 0;
 	}
 	
@@ -145,30 +145,31 @@ int InsertNewObject(HeaderD** pStruct7, char* pNewID, unsigned long int NewCode)
 		return 0;
 	
 	newObj->pID = (char*)malloc(sizeof(pNewID));
-	strcpy(newObj->pID, pNewID);
+	if (newObj->pID == NULL)
+		return 0;
+	
+	strcpy(newObj->pID, pNewID);		// add pID
+	newObj->Code = NewCode;				// add Code
 
-	newObj->Code = NewCode;
-
-	time_t RawTime = time(&RawTime);
+	time_t RawTime = time(&RawTime);	// add Time
 	if (!GetTime1(RawTime, &newObj->sTime1))
 		return 0;
 	
 
-	if (exists < 0) {	// added element needs to be first
-		if ((Object3*)pS7->pObject != NULL)
+	if (exists == first) {	// added element needs to be first
+		if ((Object3*)pS7->pObject)		// checking if added object is the only one in the list
 			newObj->pNext = (Object3*)pS7->pObject;
 		else
 			newObj->pNext = NULL;
-		pS7->pObject = (void*)newObj;
+		pS7->pObject = (void*)newObj;	// the pointer to the first object in the header must be adjusted
 	}
-	else {
-		if (foundObj->pNext == NULL)
+	else {	// added element is somewhere in the middle or last
+		if (!foundObj->pNext)
 			newObj->pNext = NULL;
 		else
 			newObj->pNext = foundObj->pNext;
 		foundObj->pNext = newObj;
 	}
-	// get time
 	
 	printf("%-15s %-10lu %02d:%02d:%02d\n", newObj->pID, newObj->Code, newObj->sTime1.Hour, newObj->sTime1.Minute, newObj->sTime1.Second);
 	
@@ -178,42 +179,43 @@ int InsertNewObject(HeaderD** pStruct7, char* pNewID, unsigned long int NewCode)
 	return 1;
 }
 
+// function for deleting existing objects
 Object3* RemoveExistingObject(HeaderD** pStruct7, char* pExistingID) {
 	if (!checkFormat(pExistingID))	// check if input char can even exist
 		return 0;
-	
+
 	HeaderD* pS7 = *pStruct7;
-	
-	while (pExistingID[0] > pS7->cBegin)
+
+	while (pS7 && pExistingID[0] > pS7->cBegin)
 		pS7 = pS7->pNext;
 
-	if (pExistingID[0] != pS7->cBegin)
+	if (!pS7 || pExistingID[0] != pS7->cBegin)
 		return 0;
 
 	Object3* obj;
-	int exists = -1;
+	int exists = first;
 	obj = findObject((Object3*)pS7->pObject, pExistingID, &exists);
 
-	if (exists != 1)
+	if (exists != present)	// if the object is not in the list there is nothing to delete
 		return 0;
 	HeaderD* buffer = pS7;
 
 	if ((Object3*)pS7->pObject == obj) {	// found object is first in the list
-		if (obj->pNext == NULL) {			// edge case, found object is the only element in list
+		if (!obj->pNext) {					// edge case, found object is the only element in list
 			pS7->pObject = NULL;
 
-			if (pS7->pPrior != NULL && pS7->pNext != NULL) {	// header is not last and not first
-				pS7->pPrior->pNext = pS7->pNext;	// jump the header were trying to delete
+			if (pS7->pPrior && pS7->pNext) {				// header is not last and not first
+				pS7->pPrior->pNext = pS7->pNext;			// jump the header were trying to delete
 				pS7->pNext->pPrior = pS7->pPrior;
 				while(pS7->pPrior != NULL)
 					pS7 = pS7->pPrior;		// rewind to the first header
 			}
-			else if (pS7->pPrior != NULL) {							// header is last
+			else if (pS7->pPrior) {							// header is last
 				pS7->pPrior->pNext = NULL;
-				while (pS7->pPrior != NULL)
+				while (pS7->pPrior)
 					pS7 = pS7->pPrior;		// rewind to the first header
 			}
-			else if (pS7->pNext != NULL) {							// header is first
+			else if (pS7->pNext) {							// header is first
 				pS7->pNext->pPrior = NULL;
 				pS7 = pS7->pNext;			// the first header is now ->pNext			
 			}
@@ -233,7 +235,7 @@ Object3* RemoveExistingObject(HeaderD** pStruct7, char* pExistingID) {
 		Object3* prevObj = (Object3*)pS7->pObject;
 		while (prevObj->pNext != obj)	// need to find the previous object to the one we want to remove
 			prevObj = prevObj->pNext;
-		if (obj->pNext == NULL)
+		if (!obj->pNext)
 			prevObj->pNext = NULL;			
 		else
 			prevObj->pNext = obj->pNext;
@@ -242,14 +244,15 @@ Object3* RemoveExistingObject(HeaderD** pStruct7, char* pExistingID) {
 	return obj;
 }
 
-
+unsigned long int randomNumber() {
+	return rand() * 30;	// RAND_MAX is 32767, so the result is multiplied by 30 to get a 9 digit number as a limit
+}
 
 
 int main()
 {
 	srand(time(NULL));   // Initialization, use to generate Code
 	// Kirjutage lähtestruktuuri genereeriv lause. See on:
-	//printf("max is %ld\n", RAND_MAX);
 	// g) Struct7 puhul:
 	HeaderD *pStruct = GetStruct7(3, 35);
 	if (!pStruct)							// exit if faulty data was given
@@ -286,9 +289,6 @@ int main()
 				break;
 		}
 	}
-	
-	
-	
 	
 	return 0;
 }
